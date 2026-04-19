97e580fa94d35029ed5a937c735beebecd977c88
OliverApelqvistEriksson <125409519+OliverApelqvistEriksson@users.noreply.github.com>
2026-04-19 15:45:17 +0200

    Add files via upload
    Behöver fortfarande LCD och funktioner relaterade till snurrandet av rotary encoder.

--- elproj.ino
+++ elproj.ino
@@ -28,6 +28,13 @@ int setupknapp_state = 0;
 int startknapp_state = 0;
 int lockinknapp_state = 0;
 
+unsigned long itryckt_tid = 0;
+unsigned long sluttid = 0;
+unsigned long starttid = 0;
+
+String mode = "";
+
+
 void setup() {
   //snurrknapp
   encoder.setPosition(10 / ROTARYSTEPS); // start with the value of 10.
@@ -45,12 +52,13 @@ void setup() {
   pinMode(lockinknapp, INPUT);
 
   //startsignal
-  lcd.print("Startar!");
+  lcd.print("Startar...");
   delay(1000);
   spela_buzzer(100, 200);
   spela_buzzer(100, 500);
   lcd.clear();
 }
+
 
 void spela_buzzer(int buzztid, int paustid) {
   digitalWrite(buzzer, HIGH);
@@ -58,6 +66,7 @@ void spela_buzzer(int buzztid, int paustid) {
   digitalWrite(buzzer, LOW);
   delay(paustid);
 }
+
 
 int kolla_knappar() {
   setupknapp_state = digitalRead(setupknapp);
@@ -94,10 +103,11 @@ int kolla_knappar() {
 
   return 67;
 }
+
+
 unsigned long knapptid(int knapp, unsigned long sys_tid) {
-  unsigned long sluttid = 0;
   unsigned long starttid = sys_tid;
-  int knapp_state = digitalRead(knapp)
+  int knapp_state = digitalRead(knapp);
   while (knapp_state == HIGH) {
     sluttid = sys_tid;
   }
@@ -105,30 +115,140 @@ unsigned long knapptid(int knapp, unsigned long sys_tid) {
   return totaltid;
 }
 
-void pomodorocykel() {
 
+void pris(int antal) {
+  //sätt in dispenserfunktion
 
 }
 
 
-int steg = 1;
+bool kontrollera_mobil() {
+  // ersätt den här
+  return true;
+}
+
+
+void varning() {
+  delay(1000);
+  if (kontrollera_mobil() == false) {
+    return;
+  }
+
+  for (int i=0; i<10; i++) {
+    spela_buzzer(200, 200);
+  }
+  
+  if (kontrollera_mobil() == false) {
+    return;
+  }
+
+  for (int i=0; i<1; i++) {
+    spela_buzzer(3000, 0);
+  }
+  
+  if (kontrollera_mobil() == false) {
+    return;
+  }
+}
+
+
+void pomodorocykel(bool duktig, int priser) { //kan ta in en string som enkapsulerar båda istället
+  //timerstart
+  unsigned long cykelstart = millis();
+  //kontrollera mobil
+  while (true) {
+    unsigned long cykeltid = millis() - cykelstart;
+    
+    if (kontrollera_mobil() == false) {
+      varning();
+    }
+    
+    if (cykeltid <= 1500000) {
+      break;
+    }
+  }
+  pris(priser);
+}
+
+
+void pomodoromaskin(int loopar, String mode) { //skulle eventuellt kunna ha att den tar in en str som typ "mode".
+  for (loopar > 0; loopar--;) {
+    pomodorocykel(false, 1);
+  }
+
+}
+
+
 void loop() {
   unsigned long sys_tid = millis();
-  
+  int igang = 1;
+  int steg = 1;
   //huvudsystemet
+  
+  while (igang == 1) {
+
+    //steg 1: Vilka inställningar?
+    while (steg == 1) {
+    
+      int itryckt = kolla_knappar();
+      // om knapp itryckt: hur länge är den itryckt? 
+      if (itryckt =! 67) {
+        unsigned long itryckt_tid = knapptid(itryckt, sys_tid);
+        // ger tid i millisekunder som den är itryckt.
+      }
+
+      //behövs kod för rot.encoder här.
+      //mode = ""; mode ändras till något här som kommer användas till
+
+      if (itryckt == 12 && itryckt_tid >= 5000) { //setupknapp
+        // servo_öppnare() // typ nåt sånt här som låter en ladda upp med dricka.
+      }
+      
+      else if (itryckt == 19 && itryckt_tid >= 50) { //selectknapp
+        steg = 2; // den kommer genom kolla_knappar registrera snurrningarna och sedan trycker man select för att gå vidare.
+      }
+
+    }
+
+    //steg 2: hur många cykler? Rot. encoder snurrar för att bestämma antal.
+    while (steg == 2) {
+
+      int itryckt = kolla_knappar();
+      // om knapp itryckt: hur länge är den itryckt? 
+      if (itryckt != 67) {
+        unsigned long itryckt_tid = knapptid(itryckt, sys_tid);
+        // ger tid i millisekunder som den är itryckt.
+      }
+
+      //grej för rot. encoder här oxå.
+      
+      if (itryckt == 12 && itryckt_tid >= 50) { //setupknapp
+        steg = 1;
+      }
+      
+      else if (itryckt == 19 && itryckt_tid >= 50) { //selectknapp
+        steg = 3;
+      }
+    
+    }
+
+    //steg 3: kör programmet enligt instruktioner från 1-2.
+    while (steg == 3) {
+      int loopar = 5; // ska kunna ändras i steg 2
+      //int antal_pomodorare = 1; //ska kunna ändras i steg 1
+      //bool duktig = false; //ska kunna ändras i steg 1
+      // kör pomodoro här.
+      pomodoromaskin(loopar, mode); // lägg in de andra variablarna allt eftersom.
+      steg = 67;
+      igang = 0;
+    }
+  }
+ 
+ 
+  //Algoritm:
   //1. välj antal cykler
   //1. välj om lockin-knapp
-  while (steg == 1) {
-    int itryckt kolla_knappar();
-    // om knapp itryckt: hur länge är den itryckt? 
-    
-    if (itryckt =! 67) {
-      unsigned long itryckt_tid = knapptid(itryckt)
-      // ger tid i millisekunder som den är itryckt.
-    }
-    // annars om roterare snurrad: byter värde automatiskt.
-
-  }
+  
   //2. starta system
 
 
@@ -142,6 +262,11 @@ void loop() {
   //här väljs antingen godisdispenser, drycklock eller båda när tiden är slut.
 
   //<---- till hit. 
+
+  //coola extrasaker:
+  // duktig-program: tar bort krav på mobilhållare:
+  //Co-op: flera godisutmatningar
+  
 
 
   //avslutningsmeddelande efter och sedan startas programmet om.
